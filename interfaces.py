9fd4ac3e2802a5a208dd3b0447a212ec09d0a39d
Andrew Sterner <andrewjohnsterner@gmail.com>
2017-08-10 15:00:08 -0700

    improved enet gpib control. Added serial_poll function

--- interfaces.py
+++ interfaces.py
@@ -161,15 +161,18 @@ class PrologixEnetInterface(BaseInterface):
         return self._gpib_addr
 
 MAV = 0x10
+ERR = 0x4
 class TempPrologixEnetInterface(SocketInterface):
     """ works for only one device at a time """
     def __init__(self, gpib_addr, addr, timeout=10000, source_address=None):
         check_gpib(gpib_addr)
         super(TempPrologixEnetInterface, self).__init__(addr, 1000, source_address)
         self.write_raw("++mode 1\n++auto 0\n++addr " + str(gpib_addr) + '\n'+ '++eos 0\n*CLS;*WAI;*SRE 32\n')
+        # TODO: see about removing this test
         try:
             while True:
-                print("TRY:" + self._read_raw())
+                print("TRY")
+                print("READ:" + self._read_raw())
         except InterfaceTimeoutError:
             print("DONE")
             self.timeout = timeout
@@ -180,13 +183,12 @@ class TempPrologixEnetInterface(SocketInterface):
         runs = int(timeout // 50 + 1)
         for i in range(runs):
             try:
-                self.write_raw("++spoll\n")
-                stb = int(self._read_raw().rstrip('\r\n'))
-                time.sleep(.05)
-                if stb == MAV:
+                stb = self.serial_poll()
+                if (stb & MAV) == MAV:
                     self.timeout = timeout
                     self.write_raw("++read {:d}\n".format(ord('\n')))
                     return self._read_raw()
+                time.sleep(.05)
             except InterfaceTimeoutError:
                 pass
         self.timeout = timeout
@@ -195,95 +197,17 @@ class TempPrologixEnetInterface(SocketInterface):
     def _read_raw(self, size=None):
         return super(TempPrologixEnetInterface, self).read_raw(size)
 
-    def __del__(self):
-        self.write_raw("++rst\n")
+    def serial_poll(self):
+        self.write_raw("++spoll\n")
+        stb = int(self._read_raw().rstrip('\r\n'))
         
+        if (stb & ERR) == ERR:
+            warnings.warn("Device has error bit set")
+        return stb
 
-# class PrologixController(SocketInterface):
-#     """ interface for prologix gpib enet controller """
-#     _PORT = 1234
-#     def __init__(self, ip, timeout=10.0, source_address=None):
-#         super(PrologixController, self).__init__((ip, self._PORT),
-#                                                  timeout=timeout,
-#                                                  source_address=source_address)
-#         self._interfaces = dict()
-#         self._active_interface = None
-#         self.write_raw("++mode 1\n++auto 1\n")
+    # def __del__(self):
+    #     self.write_raw("++rst\n")
 
-#     def _activate(self, gpib_addr):
-#         """ activate device with given address """
-#         check_gpib(gpib_addr)
-#         assert gpib_addr in self._interfaces, "Interface is not open"
-#         if self._active_interface != gpib_addr:
-#             self.write_raw("++addr {}\n".format(gpib_addr))
-#             self._active_interface = gpib_addr
-
-#     def open_interface(self, gpib_addr):
-#         """ returns a device interface for the given gpib_addr """
-#         return PrologixInterface(gpib_addr, self)
-
-#     def register(self, prologix_interface):
-#         """ registers device with controller """
-#         gpib_addr = prologix_interface.gpib_addr
-#         check_gpib(gpib_addr)
-#         if gpib_addr in self._interfaces:
-#             raise ValueError("Prologix Controller has open handle to GPIB::{}".format(gpib_addr))
-#         self._interfaces[gpib_addr] = prologix_interface
-
-#     def close_interface(self, gpib_addr):
-#         """ closes handle """
-#         self._interfaces[gpib_addr].close(_from_controller=True)
-#         del self._interfaces[gpib_addr]
-
-
-#     def gpib_write_raw(self, gpib_addr, message):
-#         """ helper function for child Interface write_raw """
-#         self._activate(gpib_addr)
-#         return self.write_raw(message)
-
-#     def gpib_read_raw(self, gpib_addr):
-#         """ helper function for child Interface read_raw """
-#         self._activate(gpib_addr)
-#         return self.read_raw()
-
-
-
-# class PrologixInterface(BaseInterface):
-#     """
-#     Interface for a single device on prologix ethernet to gpib controller
-#     """
-#     # TODO: lots of testing
-#     def __init__(self, gpib_addr, controller):
-#         self.gpib_addr = check_gpib(gpib_addr)
-#         self._controller = controller
-#         self._open = True
-#         self._controller.register(self)
-
-#     def write_raw(self, message):
-#         """ write to device """
-#         assert self._open, "Interface closed"
-#         return self._controller.gpib_write_raw(self.gpib_addr, message)
-
-#     def read_raw(self, size=None):
-#         """ read from device """
-#         assert self._open, "Interface closed"
-#         return self._controller.gpib_read_raw(self.gpib_addr)
-
-#     def close(self, _from_controller=False):
-#         """
-#         closes device. must be called before handle reused
-
-#         _from_controller used to prevent infinite loop and allow call from self or controller
-#         """
-#         if _from_controller:
-#             self.gpib_addr = None
-#             self._controller = None
-#             self._open = False
-#         else:
-#             self._controller.close_interface(self.gpib_addr)
-
-#     def __del__(self):
-#         self.close()
 
 def check_gpib(gpib_addr):
     """
